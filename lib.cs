1c62e56d2f451bc34af4074a3d276cc11174cc65
Aykut <aykut.tokatli@gmail.com>
2022-10-16 20:32:56 +0300

    v9.26 fresh installation
    v9.26 kuruldu ve sadece ChartSistem klasörü ve lib.cs dosyası commitlendi.
    Dokunulmamış fresh installation'a dönmek istersen bu revizyonu kullan

---
+++ lib.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.Drawing.Drawing2D;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+using System.Xml;
+using System.CodeDom.Compiler;
+using System.Security;
+using System.Security.Cryptography;
+using System.Collections.Concurrent;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Globalization;
+using ideal;
+
+namespace ideal
+{
+    public class Lib
+    {
+        [DllImport("kernel32.dll")]
+
+        public static extern UInt64 GetTickCount64();
+		
+    }
+}
